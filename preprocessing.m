e70ca32efb7b393f95803089b4adcfc7aaa13132
ktomlee <ktomlee@gmail.com>
2018-03-02 14:59:44 -0800

    FFT complete

--- preprocessing.m
+++ preprocessing.m
@@ -1,4 +1,22 @@
 %Pre-processing
 
-patient = readtable('1002603.txt');
+ecg = load('1002867.txt');
+
+L = length(ecg);
+fs = 500;
+fn = 250;
+t = (1:L);
+Fv = linspace(0, 1, fix(L/2)+1)*fn;
+Iv = 1:length(Fv);
+
+
+%row, column
+leadI = ecg((1:L),2);
+leadII = ecg((1:L),3);
+leadIII = ecg((1:L),4);
+
+fleadI = fft(leadI)/L;
+
+%plot(t,leadI);
+plot(Fv, abs(fleadI(Iv))*2)
 
