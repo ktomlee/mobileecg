0e3a0c3fb648eb3a673718be273b0033c4a09aae
ktomlee <ktomlee@gmail.com>
2018-03-26 19:39:52 -0700

    S and R wave Detection

--- multires_db2.m
+++ multires_db2.m
@@ -18,18 +18,27 @@ while i < 13
     db2rec{1,i}(4:5,:) = db2{1,i}(4:5,:);
     ydb2{1,i} = imodwt(db2rec{1,i},'db2');
 
-    [peaks{1,i},locs{1,i}] = findpeaks(ydb2{1,i}(1,:),t,'MinPeakHeight',50,'MinPeakDistance',0.300);
+    [peaks{1,i},locs{1,i}] = findpeaks(ydb2{1,i}(1,:),t,'MinPeakHeight',30,'MinPeakDistance',0.300);
+    [speaks{1,i},slocs{1,i}] = findpeaks((-ydb2{1,i}(1,:)),t,'MinPeakHeight',30,'MinPeakDistance',0.300);
     
-    figure
+    figure(i)
+    clf
     subplot(1,2,1);
     plot(t,ydb2{1,i}(1,:))
     hold on
     plot(locs{1,i},peaks{1,i},'ro')
+    hold on
+    plot(slocs{1,i},-speaks{1,i},'bo')
+    grid
+    %legend('ECG Signal','R-waves','S-waves','Location','northeastoutside')
     xlabel('Seconds')
 
     subplot(1,2,2);
     plot(t, ecg(:,i+1));
+     grid
     xlabel('Seconds')
     
+    
     i = i + 1;
+    
 end
